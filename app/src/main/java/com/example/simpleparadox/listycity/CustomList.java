1361698f812947ceb24afbea5bdcbcc2919ecce6
Iyury1 <iyurychu@ualberta.ca>
2022-03-08 19:00:44 -0700

    added countCities()

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -54,8 +54,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int getCount() {
-        return 0;
-//        return cities.size();
+        return cities.size();
     }
 
     public boolean hasCity(City city) {
