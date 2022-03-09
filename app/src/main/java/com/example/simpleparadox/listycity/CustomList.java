475dae43094b9fd76e80c1f37f663d6f67576620
Iyury1 <iyurychu@ualberta.ca>
2022-03-08 18:57:29 -0700

    added deleteCity()

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -62,8 +62,8 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void deleteCity(City city) {
-//        if (cities.contains(city)) {
-//            cities.remove(city);
-//        }
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
     }
 }
